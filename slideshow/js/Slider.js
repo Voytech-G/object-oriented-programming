2116b98aabdac6b9cc21094ff52659fb0e21f109
Wojciech Gwardjak <wojciech.gwardjak@ziflow.com>
2024-05-11 10:58:42 +0200

    Private slideshow properties and methods

--- slideshow/js/Slider.js
+++ slideshow/js/Slider.js
@@ -1,5 +1,12 @@
 import Slide from "./Slide.js";
 export default class Slider {
+  #slideImage;
+  #slideTitle;
+  #slideDots;
+  #slideTime;
+  #slideActive;
+  #slides;
+  #slideInterval;
   constructor(
     slides,
     sliderImageSelector,
@@ -8,58 +15,56 @@ export default class Slider {
     slideTime = 2000,
     slideActive = 0
   ) {
-    this.slideImage = document.querySelector(sliderImageSelector);
-    this.slideTitle = document.querySelector(sliderTitleSelector);
-    this.slideDots = [...document.querySelectorAll(sliderDotsSelector)];
-    this.slideTime = slideTime;
-    this.slideActive = slideActive;
-    this.slides = [];
-    this.slideInterval = null;
+    this.#slideImage = document.querySelector(sliderImageSelector);
+    this.#slideTitle = document.querySelector(sliderTitleSelector);
+    this.#slideDots = [...document.querySelectorAll(sliderDotsSelector)];
+    this.#slideTime = slideTime;
+    this.#slideActive = slideActive;
+    this.#slides = [];
+    this.#slideInterval = null;
 
     for (const slide of slides) {
-      this.addSlide(new Slide(slide.img, slide.text));
+      this.#addSlide(new Slide(slide.img, slide.text));
     }
-    window.addEventListener("keydown", (e) => this.keyDownSlide(e));
+    window.addEventListener("keydown", (e) => this.#keyDownSlide(e));
   }
-  addSlide(slide) {
-    this.slides.push(slide);
+  #addSlide(slide) {
+    this.#slides.push(slide);
   }
   startSlider() {
-    this.slideInterval = setInterval(
-      (e) => this.changeSlide(e),
-      this.slideTime
+    this.#slideInterval = setInterval(
+      (e) => this.#changeSlide(e),
+      this.#slideTime
     );
   }
-  changeDot() {
-    const activeDot = this.slideDots.findIndex((dot) =>
+  #changeDot() {
+    const activeDot = this.#slideDots.findIndex((dot) =>
       dot.classList.contains("slideshow-dot--active")
     );
-    console.log(activeDot);
-    this.slideDots[activeDot].classList.remove("slideshow-dot--active");
-    this.slideDots[this.slideActive].classList.add("slideshow-dot--active");
+    this.#slideDots[activeDot].classList.remove("slideshow-dot--active");
+    this.#slideDots[this.#slideActive].classList.add("slideshow-dot--active");
   }
-  changeSlide() {
-    console.log(this);
-    this.slideActive++;
-    if (this.slideActive === this.slides.length) {
-      this.slideActive = 0;
+  #changeSlide() {
+    this.#slideActive++;
+    if (this.#slideActive === this.#slides.length) {
+      this.#slideActive = 0;
     }
-    this.slideImage.src = this.slides[this.slideActive].image;
-    this.slideTitle.textContent = this.slides[this.slideActive].text;
-    this.changeDot();
+    this.#slideImage.src = this.#slides[this.#slideActive].image;
+    this.#slideTitle.textContent = this.#slides[this.#slideActive].text;
+    this.#changeDot();
   }
-  keyDownSlide(e) {
+  #keyDownSlide(e) {
     if (e.keyCode == 37 || e.keyCode == 39) {
-      clearInterval(this.slideInterval);
-      e.keyCode == 37 ? this.slideActive-- : this.slideActive++;
-      if (this.slideActive === this.slides.length) {
-        this.slideActive = 0;
-      } else if (this.slideActive < 0) {
-        this.slideActive = this.slides.length - 1;
+      clearInterval(this.#slideInterval);
+      e.keyCode == 37 ? this.#slideActive-- : this.#slideActive++;
+      if (this.#slideActive === this.#slides.length) {
+        this.#slideActive = 0;
+      } else if (this.#slideActive < 0) {
+        this.#slideActive = this.#slides.length - 1;
       }
-      this.slideImage.src = this.slides[this.slideActive].image;
-      this.slideTitle.textContent = this.slides[this.slideActive].text;
-      this.changeDot();
+      this.#slideImage.src = this.#slides[this.#slideActive].image;
+      this.#slideTitle.textContent = this.#slides[this.#slideActive].text;
+      this.#changeDot();
       this.startSlider();
     }
   }
